6a5276bfc7b00b0c7bd737dbd704bdcce5ad8dc6
Alfredo C <alfredo_cardenas@live.com>
2026-05-07 17:52:13 -0600

    feat: add the new+create controller methods, with the new view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,35 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new   # needed to display the form fields
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+     redirect_to @product
+    else
+     render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_params
+      params.expect(product: [ :name ])
+    end
 end
+
+=begin
+      Prefix  Verb    URI Pattern                  Controller#Action
+        root  GET     /                              products#index
+    products  GET     /products(.:format)            products#index
+              POST    /products(.:format)            products#create
+ new_product  GET     /products/new(.:format)        products#new
+edit_product  GET     /products/:id/edit(.:format)   products#edit
+     product  GET     /products/:id(.:format)        products#show
+              PATCH   /products/:id(.:format)        products#update
+              PUT     /products/:id(.:format)        products#update
+              DELETE  /products/:id(.:format)        products#destroy
+=end
